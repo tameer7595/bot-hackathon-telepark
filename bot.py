31bcbde30373efde216eb72d54ae02dac7982671
omarburqan <omar.b__95@hotmail.com>
2019-10-29 11:38:48 +0200

    2.1 request list

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import pymongo
 import logging
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Updater
+import time
 
 
 def start(update: Update, context: CallbackContext):
@@ -25,7 +26,29 @@ def users(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=chat_id, text=res)
 
 
-def creat_users():
+def book_tmrw(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    logger.info(f"= Got on chat #{chat_id}: {text!r}")
+    db = client.get_database('parking_db')
+    requests = db.get_collection('request_list')
+    requests.replace_one(
+        {"chat_id": chat_id},
+        {"chat_id": chat_id, "time": time.time()},
+        upsert=True
+    )
+    res = 'we received your request, we will reply to you soon'
+    context.bot.send_message(chat_id=chat_id, text=res)
+
+
+def create_request_list():
+    db = client.get_database('parking_db')
+    requests = db.get_collection('request_list')
+    requests.delete_many({})
+    requests.create_index([('user_id', pymongo.ASCENDING)])
+
+
+def create_users():
     liwaa_id = 1044776988
     tameer_id = 836471985
     omar_id = 574225603
@@ -46,8 +69,8 @@ def creat_users():
 
 if __name__ == '__main__':
     client = pymongo.MongoClient()
-    creat_users()
-
+    create_users()
+    create_request_list()
     logging.basicConfig(
         format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
         level=logging.INFO)
@@ -62,6 +85,9 @@ if __name__ == '__main__':
 
     users_handler = CommandHandler('users', users, )
     dispatcher.add_handler(users_handler)
+
+    book_tomorrow_handler = CommandHandler('book_tmrw', book_tmrw, )
+    dispatcher.add_handler(book_tomorrow_handler)
 
     logger.info("* Start polling...")
     updater.start_polling()  # Starts polling in a background thread.
